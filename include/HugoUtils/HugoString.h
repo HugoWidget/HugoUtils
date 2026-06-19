7a7ae04271458006cd38c53ce9a62a305013e96e
howdy213 <howdy213@foxmail.com>
2026-06-19 08:55:56 +0800

    fix(HugoString.h): use Unicode escape sequences

--- include/HugoUtils/HugoString.h
+++ include/HugoUtils/HugoString.h
@@ -2,8 +2,8 @@
 namespace HugoUtils
 {
 	namespace FreezeApiInvalidStrings {
-		constexpr auto Str1 = L"后再冰";
-		constexpr auto Str2 = L"不被冰";
-		constexpr auto Str3 = L"冻\"";
+		constexpr auto Str1 = L"\u540E\u518D\u51B0";   // "后再冰"
+		constexpr auto Str2 = L"\u4E0D\u88AB\u51B0";   // "不被冰"
+		constexpr auto Str3 = L"\u51BB\u0022";         // "冻\""
 	}
 }
